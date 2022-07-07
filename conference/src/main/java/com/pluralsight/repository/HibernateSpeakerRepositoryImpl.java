4162c24f961167f87c9a3fb67e803c7da0f4a7b3
negi-saurabh <saurabh.negi89@gmail.com>
2022-07-07 08:23:31 +0200

    07/07 Spring demo

---
+++ conference/src/main/java/com/pluralsight/repository/HibernateSpeakerRepositoryImpl.java
@@ -0,0 +1,18 @@
+package com.pluralsight.repository;
+
+import com.pluralsight.model.Speaker;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HibernateSpeakerRepositoryImpl implements SpeakerRepository {
+    @Override
+    public List<Speaker> findAll() {
+        List<Speaker> speakers = new ArrayList<>();
+        Speaker speaker = new Speaker();
+        speaker.setFirstName("sam");
+        speaker.setLastName("Manekshaw");
+        speakers.add(speaker);
+        return speakers;
+    }
+}
